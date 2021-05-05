3bc62a088fd863deb3dcddd6ab4117b1b63163bd
Dmitry <fedoseev.2001@list.ru>
2021-05-05 22:33:21 +0300

    init

--- lab2/num3.py
+++ lab2/num3.py
@@ -1,130 +1,70 @@
-import re
-import datetime
+def f23(table_list):
+
+    new1_list = []
+    for i in range(len(table_list)):
+        new1_list.append([])
+        for j in range(len(table_list[i])):
+            if table_list[i][j] != None and table_list[i][j] != table_list[i][j - 1]:
+                new1_list[i].append(table_list[i][j])
+
+    new1_list.remove([])
+    new_list = []
+    new_list.append([])
+    new_list[0].append(new1_list[0][0])
+    new_list[0].append(new1_list[0][1])
+    new_list[0].append(new1_list[0][2])
+    new_list[0].append(new1_list[0][3])
+
+    m = 0
+    for i in range(len(new1_list)):
+        chck = True
+        for k in range(len(new_list)):
+            if new1_list[i][0] == new_list[k][0]:
+                chck = False
+        if chck:
+            m += 1
+            new_list.append([])
+            for j in range(len(new1_list[i])):
+                new_list[m].append(new1_list[i][j])
 
 
-class Table:
-    def __init__(self, name, num0, date, num1, num2):
-        self.name = name
-        self.num0 = num0
-        self.date = date
-        self.num1 = num1
-        self.num2 = num2
+    num_empty = 0
+    for i in range(len(new_list)):
+        if not new_list[i]:
+            num_empty += 1
 
-    def __str__(self):
-        return format(self.name, self.num0, self.date, self.num1, self.num2)
+    for i in range(num_empty):
+        new_list.remove([])
 
-    def __eq__(self, other):
-        if isinstance(other, Table):
-            return (self.name == other.name and
-                    self.num0 == other.num0 and
-                    self.date == other.date and
-                    self.num1 == other.num1 and
-                    self.num2 == other.num2)
-        return NotImplemented
+    table_transponded = []
 
-    def getName(self):
-        return self.name
+    for i in range(len(new_list)):
+        if new_list[i][1] == '1':
+            new_list[i][1] = 'Y'
+        elif new_list[i][1] == '0':
+            new_list[i][1] = 'N'
 
-    def getNum0(self):
-        return self.num0
+        tab1 = str(new_list[i][0]).split(' ')
+        new_list[i][0] = tab1[1] + ' ' + tab1[0]
 
-    def getDate(self):
-        return self.date
+        tab = str(new_list[i][2]).split('.')
+        new_list[i][3] = round(float(new_list[i][3]), 1)
+        new_list[i][2] = tab[2] + '-' + tab[1] + '-' + tab[0]
+        new_list[i][3] = str(new_list[i][3])
 
-    def getNum1(self):
-        return self.num1
+    for i in range(len(new_list[i])):
+        table_transponded.append([])
+        for j in range(len(new_list)):
+            if j < len(new_list) and i < len(new_list[0]):
+                table_transponded[i].append(new_list[j][i])
 
-    def getNum2(self):
-        return self.num2
+    return table_transponded
 
 
-newTable = []
-nameTable = []
-surnameTable = []
-sexTable = []
-dateTable = []
-numTable = []
-
-
-def restruct():
-
-    for i in range(len(newTable)):
-        nameTable.append(newTable[i].getName())
-
-    for i in range(len(newTable)):
-        surnameTable.append(newTable[i].getNum0())
-
-    for i in range(len(newTable)):
-        sexTable.append(newTable[i].getDate())
-
-    for i in range(len(newTable)):
-        dateTable.append(newTable[i].getNum1())
-
-    for i in range(len(newTable)):
-        numTable.append(newTable[i].getNum2())
-
-
-def prnt():
-    for i in range(len(nameTable)):
-        print('|' + nameTable[i], end='')
-        print(' ' + surnameTable[i], end='|')
-
-    print('\n' + '-' * len(newTable)*16)
-
-    for i in range(len(sexTable)):
-        print('|' + sexTable[i] + '|', end='\t\t\t\t\t')
-
-    print('\n' + '-' * len(newTable)*16)
-
-    for i in range(len(dateTable)):
-        print('|' + dateTable[i] + '|', end='\t')
-
-    print('\n' + '-' * len(newTable)*16)
-
-    for i in range(len(numTable)):
-        print('|' + numTable[i] + '|', end='\t\t\t\t')
-
-
-def sort():
-    i = 0
-
-    while i < len(newTable) - 1:
-
-        if newTable[i] == newTable[i + 1] and i < len(newTable):
-            del newTable[i]
-            # print('done')
-        else:
-            i += 1
-
-
-def f23():
-    string = input('Enter info: ').split()
-
-    if 6 > len(string) > 1:
-
-        if len(string) == 5:
-
-            string[2] = "Y" if string[2] == '1' else "N"
-            newTable.append(Table(string[0], string[1], string[2], re.sub('\.', '-', str(string[3])), str(string[4])[0:3]))
-
-            # print(newTable[0])
-            f23()
-        else:
-            return -1
-
-    elif string[0].__eq__('print'):
-        sort()
-        restruct()
-        prnt()
-
-    else:
-        return 0
-
-
-f23()
-
-'''
-Валерий Андреев 1 22.08.2001 0.376
-Николай Васин 1 21.04.1989 0.477
-Кондратий Теркин 0 22.02.1999 0.877
-'''
+f23([['Зомегянц Святослав', '1', None, '2001.05.25', '0.333', '0.333'],
+ ['Гумевич Валерий', '1', None, '2001.12.16', '0.332', '0.332'],
+ [None, None, None, None, None, None],
+ ['Тобечев Максим', '0', None, '2000.02.20', '0.541', '0.541'],
+ ['Зомегянц Святослав', '1', None, '2001.05.25', '0.333', '0.333'],
+ ['Бамочян Захар', '1', None, '1999.04.04', '0.929', '0.929'],
+ ['Зомегянц Святослав', '1', None, '2001.05.25', '0.333', '0.333']])
